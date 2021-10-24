a61523e52fc915d76518db5427b7b5d58e8d11c5
Maryam-Yousaf <93052442+Maryam-Yousaf@users.noreply.github.com>
2021-10-24 09:45:31 -0700

    if number is even or odd

---
+++ even odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num=0;
+printf("enter a number");
+scanf("%d",&num);
+if(num%2==0)
+{
+printf("the number is even=%d",num);
+}
+else
+{
+printf("the number is odd=%d",num);
+ }
+}
